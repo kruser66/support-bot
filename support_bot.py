4a522862b1e12802b836fce4dda835ec68127283
kruser <kruser@yandex.ru>
2021-12-24 21:14:46 +0500

    setup DialogFlow

--- support_bot.py
+++ support_bot.py
@@ -13,17 +13,53 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+def detect_intent_texts(project_id, session_id, texts, language_code):
+    from google.cloud import dialogflow
+
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+    print("Session path: {}\n".format(session))
+
+    text_input = dialogflow.TextInput(
+        text=texts,
+        language_code=language_code
+    )
+
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+    print("Query text: {}".format(response.query_result.query_text))
+    print(
+        "Detected intent: {} (confidence: {})\n".format(
+            response.query_result.intent.display_name,
+            response.query_result.intent_detection_confidence,
+        )
+    )
+
+    return response.query_result.fulfillment_text
+
+
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Здравствуйте!')
 
 
 def echo(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
+    fulfillment_text = detect_intent_texts(
+        'kruser-support-bot',
+        1234567,
+        update.message.text,
+        'ru-RU'
+    )
+    update.message.reply_text(fulfillment_text)
 
 
 def main():
     load_dotenv()
     token = os.getenv('TG_BOT_TOKEN')
+    os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
 
     updater = Updater(token)
     dispatcher = updater.dispatcher

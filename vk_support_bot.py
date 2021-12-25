3455915a2045fb9eb7d114d0f6d708a22092ae28
kruser <kruser@yandex.ru>
2021-12-25 16:04:03 +0500

    Убрал ответ если запрос нестандартный

--- vk_support_bot.py
+++ vk_support_bot.py
@@ -3,14 +3,25 @@ import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
 from dotenv import load_dotenv
 from random import randint
+from intents import detect_intent_texts
 
 
 def support(event, vk_api):
-    vk_api.messages.send(
-        user_id=event.user_id,
-        message=event.text,
-        random_id=randint(1, 1000)
+    response_intent = detect_intent_texts(
+        project_id='kruser-support-bot',
+        session_id=event.user_id,
+        texts=event.text,
+        language_code='ru-RU'
     )
+
+    if response_intent.action == 'input.unknown':
+        return
+    else:
+        vk_api.messages.send(
+            user_id=event.user_id,
+            message=response_intent.fulfillment_text,
+            random_id=randint(1, 1000)
+        )
 
 
 if __name__ == "__main__":
